3eb8b53a97027a5e98c0fdebc74bd9d4a19fc63c
Ricard Ferrero <r.ferrero.conde@gmail.com>
2022-12-04 10:10:54 +0100

    'login' and 'logout' systems

--- quote_network/network/urls.py
+++ quote_network/network/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = 'network'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='profile')
+    path('profile/', views.profile, name='profile'),
+    path('login/', LoginView.as_view(template_name='network/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='network/logout.html'), name='logout'),
 ]
--- quote_network/quote_network/settings.py
+++ quote_network/quote_network/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login (authentication) redirections
+# (custom)
+
+LOGIN_REDIRECT_URL = 'network:index'
