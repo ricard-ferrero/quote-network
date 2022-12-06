198d6bd52cea15068efb3186eb0ac3a984291b68
Ricard Ferrero <r.ferrero.conde@gmail.com>
2022-12-06 06:30:38 +0100

    'signals.py'

--- quote_network/network/models.py
+++ quote_network/network/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from django.db.models.signals import post_save
-
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -11,9 +9,3 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'Profile: {self.user.username}'
-
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-post_save.connect(create_profile, sender=User)
